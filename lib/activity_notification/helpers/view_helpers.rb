4ff75a7c91260da8123c28a7daff3034636a36bf
simukappu <shota.yamazaki.8@gmail.com>
2016-08-20 15:23:49 +0900

    Refactor view_helper to simplify render_notifications_of method

--- lib/activity_notification/helpers/view_helpers.rb
+++ lib/activity_notification/helpers/view_helpers.rb
@@ -17,54 +17,25 @@ module ActivityNotification
     def render_notification_of target, options = {}
       return unless target.is_a? ActivityNotification::Target
 
-      partial_path = options.delete(:partial) || "index"
-      partial_root = options[:partial_root] ||
-                         "activity_notification/notifications/#{target.to_resources_name}"
-      partial      = select_path(partial_path, partial_root)
-      layout       = options[:layout].present? ?
-                       select_path(options.delete(:layout), (options[:layout_root] || "layouts")) :
-                       nil
-      locals       = (options[:locals] || {}).merge(target: target)
-
       # Prepare content for notifications index
-      notification_options = options.merge\
-                               target:  target.to_resources_name, 
-                               partial: options[:notification_partial], 
-                               layout:  options[:notification_layout]
-      case options[:index_content]
-      when :simple
-        notification_index = target.notification_index
-      when :none
-      else
-        notification_index = target.notification_index_with_attributes
-      end
-
-      if notification_index.present?
-        content_for :notification_index do
-          @target = target
-          begin
-            render_notification notification_index, notification_options
-          rescue ActionView::MissingTemplate
-            notification_options.delete(:target)
-            render_notification notification_index, notification_options
-          end
+      notification_options = options.merge target: target.to_resources_name, 
+                               partial: options[:notification_partial], layout: options[:notification_layout]
+      notification_index =
+        case options[:index_content]
+        when :simple then target.notification_index
+        when :none   then target.notifications.none
+        else              target.notification_index_with_attributes
         end
-      end
+      prepare_content_for(target, notification_index, notification_options)
 
       # Render partial index
-      begin
-        render options.merge(partial: partial, layout: layout, locals: locals)
-      rescue ActionView::MissingTemplate
-        partial_root = "activity_notification/notifications/default"
-        partial      = select_path(partial_path, partial_root)
-        render options.merge(partial: partial, layout: layout, locals: locals)
-      end
+      render_partial_index(target, options)
     end
     alias_method :render_notifications_of, :render_notification_of
 
     # Url helper methods
     #TODO Is there any other better solution?
-    #TODO Must handle devise namespace
+    # Must handle devise namespace
     def notification_path_for(notification, params = {})
       send("#{notification.target.to_resource_name}_notification_path", notification.target, notification, params)
     end
@@ -101,6 +72,33 @@ module ActivityNotification
 
       def select_path(path, root)
         [root, path].map(&:to_s).join('/')
+      end
+
+      def prepare_content_for(target, notification_index, options)
+        content_for :notification_index do
+          @target = target
+          begin
+            render_notification notification_index, options
+          rescue ActionView::MissingTemplate
+            options.delete(:target)
+            render_notification notification_index, options
+          end
+        end
+      end
+
+      def render_partial_index(target, options)
+        partial_path = options.delete(:partial) || "index"
+        partial = select_path(partial_path,
+                    options[:partial_root] || "activity_notification/notifications/#{target.to_resources_name}")
+        layout  = options[:layout].present? ?
+                         select_path(options.delete(:layout), (options[:layout_root] || "layouts")) : nil
+        locals  = (options[:locals] || {}).merge(target: target)
+        begin
+          render options.merge(partial: partial, layout: layout, locals: locals)
+        rescue ActionView::MissingTemplate
+          partial = select_path(partial_path, "activity_notification/notifications/default")
+          render options.merge(partial: partial, layout: layout, locals: locals)
+        end
       end
 
   end
