0cb2d483254536256c301feb189ee586c1597e75
simukappu <shota.yamazaki.8@gmail.com>
2016-08-28 20:17:53 +0900

    Add code documents to store_controller

--- lib/activity_notification/controllers/store_controller.rb
+++ lib/activity_notification/controllers/store_controller.rb
@@ -1,11 +1,16 @@
 module ActivityNotification
   class << self
     # Setter for remembering controller instance
+    #
+    # @param [NotificationsController, NotificationsWithDeviseController] controller Controller instance to set
+    # @return [NotificationsController, NotificationsWithDeviseController]] Controller instance to be set
     def set_controller(controller)
       Thread.current[:activity_notification_controller] = controller
     end
 
     # Getter for accessing the controller instance
+    #
+    # @return [NotificationsController, NotificationsWithDeviseController]] Controller instance to be set
     def get_controller
       Thread.current[:activity_notification_controller]
     end
@@ -20,11 +25,14 @@ module ActivityNotification
       around_filter :store_controller_for_activity_notification unless respond_to?(:around_action)
     end
 
+    # Sets controller as around action to use controller instance in models or helpers
     def store_controller_for_activity_notification
-      ActivityNotification.set_controller(self)
-      yield
-    ensure
-      ActivityNotification.set_controller(nil)
+      begin
+        ActivityNotification.set_controller(self)
+        yield
+      ensure
+        ActivityNotification.set_controller(nil)
+      end
     end
   end
 end
