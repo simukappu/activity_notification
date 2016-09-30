a77bf12bb0db3f686c4e11297f5302bf68964834
simukappu <shota.yamazaki.8@gmail.com>
2016-09-30 09:12:14 +0900

    Arrange notification email templates

--- spec/rails_app/lib/mailer_previews/mailer_preview.rb
+++ spec/rails_app/lib/mailer_previews/mailer_preview.rb
@@ -1,5 +1,13 @@
 class ActivityNotification::MailerPreview < ActionMailer::Preview
-  def send_notification_email
-    ActivityNotification::Mailer.send_notification_email(ActivityNotification::Notification.first)
+
+  def send_notification_email_single
+    target_notification = ActivityNotification::Notification.where(group: nil).first
+    ActivityNotification::Mailer.send_notification_email(target_notification)
   end
+
+  def send_notification_email_with_group
+    target_notification = ActivityNotification::Notification.where.not(group: nil).first
+    ActivityNotification::Mailer.send_notification_email(target_notification)
+  end
+
 end
