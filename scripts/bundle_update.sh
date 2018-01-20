f0c740cbababbf6a37a6d7e0e7c910a3bac54886
simukappu <shota.yamazaki.8@gmail.com>
2018-01-21 00:09:10 +0900

    Add scripts/bundle_update.sh

---
+++ scripts/bundle_update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bundle update
+BUNDLE_GEMFILE=gemfiles/Gemfile.rails-4.2 bundle update
+BUNDLE_GEMFILE=gemfiles/Gemfile.rails-5.0 bundle update
+BUNDLE_GEMFILE=gemfiles/Gemfile.rails-5.1 bundle update
