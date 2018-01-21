17dfab7c141c9b9e95e4638f337d690ea1fd767a
simukappu <shota.yamazaki.8@gmail.com>
2018-01-21 12:02:06 +0900

    Fix batch email preview in test rails app

--- spec/rails_app/lib/mailer_previews/mailer_preview.rb
+++ spec/rails_app/lib/mailer_previews/mailer_preview.rb
@@ -13,7 +13,7 @@ class ActivityNotification::MailerPreview < ActionMailer::Preview
   def send_batch_notification_email
     target = User.find_by_name('Ichiro')
     target_notifications = target.notification_index_with_attributes(filtered_by_key: 'comment.default')
-    ActivityNotification::Mailer.send_batch_notification_email(target, target_notifications)
+    ActivityNotification::Mailer.send_batch_notification_email(target, target_notifications, 'batch.comment.default')
   end
 
 end
