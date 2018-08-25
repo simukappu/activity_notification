87bf142afb9ae0979304f706fa50f53155f157ed
simukappu <shota.yamazaki.8@gmail.com>
2018-08-26 00:11:57 +0900

    Fix batch email preview in test rails app with mongoid orm

--- spec/rails_app/lib/mailer_previews/mailer_preview.rb
+++ spec/rails_app/lib/mailer_previews/mailer_preview.rb
@@ -6,12 +6,14 @@ class ActivityNotification::MailerPreview < ActionMailer::Preview
   end
 
   def send_notification_email_with_group
-    target_notification = ActivityNotification::Notification.where.not(group: nil).first
+    target_notification = ActivityNotification.config.orm == :active_record ?
+      ActivityNotification::Notification.where.not(group: nil).first :
+      ActivityNotification::Notification.where(:group_id.nin => ["", nil]).first
     ActivityNotification::Mailer.send_notification_email(target_notification)
   end
 
   def send_batch_notification_email
-    target = User.find_by_name('Ichiro')
+    target = User.find_by(name: 'Ichiro')
     target_notifications = target.notification_index_with_attributes(filtered_by_key: 'comment.default')
     ActivityNotification::Mailer.send_batch_notification_email(target, target_notifications, 'batch.comment.default')
   end
